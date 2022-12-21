21d19722f951bfa1d6e3d9e81056fdc5286f81a6
tim <superibm@mail.ru>
2022-12-21 22:23:46 +0300

    Установка системы.

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response()->json(['Welcome to API.']);
+    return response()->json(['Welcome to API 2.']);
 });
 
 Route::any('/ckfinder/connector', '\CKSource\CKFinderBridge\Controller\CKFinderController@requestAction')
