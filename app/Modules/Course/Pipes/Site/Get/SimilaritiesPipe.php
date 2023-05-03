8a30e4837940295508f04afbad310c5d128c709c
Tim <superibm@mail.ru>
2023-05-03 20:44:48 +0300

    Поправил подобные курсы.

--- app/Modules/Course/Pipes/Site/Get/SimilaritiesPipe.php
+++ app/Modules/Course/Pipes/Site/Get/SimilaritiesPipe.php
@@ -67,67 +67,71 @@ class SimilaritiesPipe implements Pipe
                 function () use ($course) {
                     $search = $course->name;
 
-                    $filters = [
-                        'search' => $search,
-                        'directions-id' => $course->directions[0]->id,
-                        'categories-id' => [$course->categories[0]->id],
-                    ];
+                    if (isset($course->directions[0]->id) && isset($course->categories[0]->id)) {
+                        $filters = [
+                            'search' => $search,
+                            'directions-id' => $course->directions[0]->id,
+                            'categories-id' => [$course->categories[0]->id],
+                        ];
 
-                    $search = Morph::get($search);
-                    $search = DB::getPdo()->quote($search);
+                        $search = Morph::get($search);
+                        $search = DB::getPdo()->quote($search);
 
-                    $query = Course::select([
-                        'id',
-                        'school_id',
-                        'image_big_id',
-                        'image_middle_id',
-                        'image_small_id',
-                        'name',
-                        'header',
-                        'header_template',
-                        'text',
-                        'link',
-                        'url',
-                        'language',
-                        'rating',
-                        'price',
-                        'price_old',
-                        'price_recurrent',
-                        'currency',
-                        'online',
-                        'employment',
-                        'duration',
-                        'duration_rate',
-                        'duration_unit',
-                        'lessons_amount',
-                        'modules_amount',
-                        'status',
-                    ])
-                    ->filter($filters)
-                    ->with([
-                        'school' => function ($query) {
-                            $query->select([
-                                'schools.id',
-                                'schools.name',
-                                'schools.link',
-                                'schools.image_logo_id',
-                            ])->where('status', true);
-                        },
-                    ])
-                    ->where('status', Status::ACTIVE->value)
-                    ->whereHas('school', function ($query) {
-                        $query->where('status', true);
-                    })
-                    ->where('id', '!=', $course->id)
-                    ->addSelect(
-                        DB::raw('MATCH(name_morphy, text_morphy) AGAINST(' . $search . ') AS relevance')
-                    )
-                    ->orderBy('relevance', 'DESC')
-                    ->limit(self::LIMIT);
+                        $query = Course::select([
+                            'id',
+                            'school_id',
+                            'image_big_id',
+                            'image_middle_id',
+                            'image_small_id',
+                            'name',
+                            'header',
+                            'header_template',
+                            'text',
+                            'link',
+                            'url',
+                            'language',
+                            'rating',
+                            'price',
+                            'price_old',
+                            'price_recurrent',
+                            'currency',
+                            'online',
+                            'employment',
+                            'duration',
+                            'duration_rate',
+                            'duration_unit',
+                            'lessons_amount',
+                            'modules_amount',
+                            'status',
+                        ])
+                            ->filter($filters)
+                            ->with([
+                                'school' => function ($query) {
+                                    $query->select([
+                                        'schools.id',
+                                        'schools.name',
+                                        'schools.link',
+                                        'schools.image_logo_id',
+                                    ])->where('status', true);
+                                },
+                            ])
+                            ->where('status', Status::ACTIVE->value)
+                            ->whereHas('school', function ($query) {
+                                $query->where('status', true);
+                            })
+                            ->where('id', '!=', $course->id)
+                            ->addSelect(
+                                DB::raw('MATCH(name_morphy, text_morphy) AGAINST(' . $search . ') AS relevance')
+                            )
+                            ->orderBy('relevance', 'DESC')
+                            ->limit(self::LIMIT);
 
-                    $items = $query->get()->toArray();
+                        $items = $query->get()->toArray();
 
-                    return Entity::toEntities($items, new CourseEntity());
+                        return Entity::toEntities($items, new CourseEntity());
+                    }
+
+                    return [];
                 }
             );
 
