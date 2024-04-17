8116ceaebb4517dafa0f69f1c183f6eaba35218a
Tim <superibm@mail.ru>
2024-04-17 18:22:35 +1000

    Оптимизировал получение учителей для административной части.

--- app/Modules/Teacher/Entities/TeacherSimple.php
+++ app/Modules/Teacher/Entities/TeacherSimple.php
@@ -45,21 +45,31 @@ class TeacherSimple extends Entity
     public ?Image $image_middle_id = null;
 
     /**
+     * Статус.
+     *
+     * @var bool|null
+     */
+    public ?bool $status = null;
+
+    /**
      * @param int|string|null $id ID записи.
      * @param string|null $name Название.
      * @param Image|null $image_small_id Изображение маленькое.
      * @param Image|null $image_middle_id Изображение среднее.
+     * @param bool|null $status Статус.
      */
     public function __construct(
         int|string|null $id = null,
         ?string         $name = null,
         ?Image          $image_small_id = null,
         ?Image          $image_middle_id = null,
+        ?bool           $status = null,
     )
     {
         $this->id = $id;
         $this->name = $name;
         $this->image_small_id = $image_small_id;
         $this->image_middle_id = $image_middle_id;
+        $this->status = $status;
     }
 }
