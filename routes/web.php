e3517a7e1f6e541774dcd338274a9532c59e076e
Tim <admin@courselandia.ru>
2024-07-19 13:55:18 +1000

    GCR-api-70 Уйти с редис на мемкеч

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,15 @@ Route::get('/', function () {
     return response()->json(['Welcome to API.']);
 });
 
+Route::get('/cache-test', function () {
+    \Cache::remember('cache-test', 86400, function () {
+        echo "GETTING...\n";
+        return 'YES!';
+    });
+
+    return response()->json(['Test Cache.']);
+});
+
 Route::get('/test', function () {
     return response()->view('test');
 });
